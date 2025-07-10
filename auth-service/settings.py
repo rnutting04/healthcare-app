23e44d8c69a5945e6161e2076783b216f955bfeb
Reubin George <reubingeorge@usf.edu>
2025-07-10 16:40:20 -0400

    sign up error fixed

--- auth-service/settings.py
+++ auth-service/settings.py
@@ -105,7 +105,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
--- auth-service/urls.py
+++ auth-service/urls.py
@@ -37,4 +37,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Serve static files from STATICFILES_DIRS in development
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
